77e7c240b8935d88f8cd5f24cc369419174320a2
JuanCarlosHernandezLomas <hernandez.lomas.juan102@gmail.com>
2024-06-10 00:36:22 -0600

    carucel completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,45 @@ const btnleft = document.querySelector(".btn-left"),
         btnright=document.querySelector(".btn-right"),
         slider=document.querySelector("#slider"),
         sliderSection=document.querySelectorAll(".slider-section");
-
-    
+        
 
 btnright.addEventListener("click", e => MoveRight())
 btnleft.addEventListener("click", e => MoveLeft())
-let contador=0;
+
+setInterval(()=>{
+    MoveRight()
+}, 5000)
+
+let contador=0,
+    count= 0,
+    widthImg = 100/sliderSection.length;
 
 function MoveRight(){
-    slider.style.transform= "translate(-33.3%)"
-
+    if(count >= sliderSection.length-1){
+        count =0;
+        contador=0;
+        slider.style.transform= `translate(-${contador}%)`;
+        slider.style.transition = "none";
+        return;
+    }
+    count++;
+        contador=contador+widthImg;
+        slider.style.transform= `translate(-${contador}%)`;
+        slider.style.transition = "all ease .6s";
 }
 
 function MoveLeft (){
-    slider.style.transform = "translate(0%)"
+    count--;
+    if(count < 0){
+        count= sliderSection.length-1;
+        contador= widthImg * (sliderSection.length-1)
+        console.log(contador)
+        slider.style.transform= `translate(-${contador}%)`;
+        slider.style.transition = "none";
+        return;
+    }
+        contador=contador-widthImg;
+        slider.style.transform= `translate(-${contador}%)`;
+        slider.style.transition = "all ease .6s";
 
 }
